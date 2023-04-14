8fc362deccf0c5d39caed4948930a3124b776218
hyewon kang <tecla326@gmail.com>
2023-04-14 00:00:51 -0700

    Developed cards section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import Footer from "@/components/footer";
 import MainJumbotron from "@/components/jumbotron/MainJumbotron";
 import testImage from "../../public/assets/images/test.jpg";
 import testImage2 from "../../public/assets/images/test2.jpg";
-
 import testImage3 from "../../public/assets/images/test3.jpg";
 import testImage4 from "../../public/assets/images/test4.jpg";
 import testImage5 from "../../public/assets/images/test5.jpg";
 import ImageList from "@/components/list/ImageList";
 import LocationSearchBar from "@/components/searchBar/LocationSearchBar";
 import CardBox from "@/components/cards/cardBox";
+
 
 export default function Home() {
 
@@ -39,10 +39,13 @@ export default function Home() {
             <CardBox />
             <MainJumbotron title="The Lottery is back!" description="30 CASH prizes and bonus 50/50 pot up to $800,000.
             You can help local pets & win big!" btnColor="bg-gray-700" image={testImage2} imagePosition="left" />
-
-           
+        <main className="flex min-h-screen flex-col items-center justify-between" />
+          <div className="w-full items-center justify-center text-sm lg:flex">
+          {/* Mount components here   */}
+          
           </div>
       <Footer />
     </div>
+  </div>
   )
 }
