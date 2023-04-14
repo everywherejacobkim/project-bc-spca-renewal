85f77b85898564441fba204955cfb0f18f3af99a
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-13 23:49:28 -0700

    Chore: test jumbotron component & clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,21 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import MainJumbotron from "@/components/jumbotron/MainJumbotron";
 import testImage from "../../public/assets/images/test.jpg";
+import testImage2 from "../../public/assets/images/test2.jpg";
 
 export default function Home() {
   return (
     <div>
       <Header />
         <main className="flex min-h-screen flex-col items-center justify-between">
-          <div className="w-full items-center justify-center text-sm lg:flex">
+          <div className="w-full items-center justify-center text-sm lg:flex-cols">
           {/* Mount components here   */}
             <MainJumbotron title="Adopt, Don't Shop" description="Every adoption, every gift brings an animal closer to their best self. Become a hero - Save a life. Create a family." btnColor="bg-gray-700" image={testImage} imagePosition="right" />
+            
+            <br />
+
+            <MainJumbotron title="The Lottery is back!" description="30 CASH prizes and bonus 50/50 pot up to $800,000.
+            You can help local pets & win big!" btnColor="bg-gray-700" image={testImage2} imagePosition="left" />
           </div>
       </main>
       <Footer />
