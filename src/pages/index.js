61ba28ffa8dd06344d517795ecf03c3987a98b8e
hyewon kang <tecla326@gmail.com>
2023-04-15 13:35:33 -0700

    Divided components and modified code for card section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Footer from "@/components/footer";
 import MainJumbotron from "@/components/jumbotron/MainJumbotron";
 import testImage from "../../public/assets/images/test.jpg";
 import testImage2 from "../../public/assets/images/test2.jpg";
-import Cards from "@/components/cards";
+import CardBox from "@/components/cards/cardBox";
+
 
 export default function Home() {
   return (
@@ -15,11 +16,12 @@ export default function Home() {
             <MainJumbotron title="Adopt, Don't Shop" description="Every adoption, every gift brings an animal closer to their best self. Become a hero - Save a life. Create a family." btnColor="bg-gray-700" image={testImage} imagePosition="right" />
             
             <br />
+            <CardBox />
 
             <MainJumbotron title="The Lottery is back!" description="30 CASH prizes and bonus 50/50 pot up to $800,000.
             You can help local pets & win big!" btnColor="bg-gray-700" image={testImage2} imagePosition="left" />
 
-            <Cards />
+           
           </div>
       </main>
       <Footer />
