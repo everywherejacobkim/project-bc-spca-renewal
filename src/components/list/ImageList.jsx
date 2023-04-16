a3d37e94c3ec6332166fa97a9dbfb3a05f167802
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-16 11:31:00 -0700

    Fix: fix styling & responsive view

--- src/components/list/ImageList.jsx
+++ src/components/list/ImageList.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const ImageList = ({ images }) => {
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
         {images.map((image, index) => (
           <div
             key={index}
--- src/components/searchBar/LocationSearchBar.jsx
+++ src/components/searchBar/LocationSearchBar.jsx
@@ -22,27 +22,27 @@ const LocationSearchBar = ({
   return (
     <form
       onSubmit={handleSubmit}
-          className={classNames(
-              "w-full rounded-xl mx-52 py-8",
-              bgColor || "bg-gray-200"
-            )}
+      className={classNames(
+        "w-full rounded-xl mx-4 sm:mx-10 lg:mx-52 py-8",
+        bgColor || "bg-gray-200"
+      )}
     >
-      <div className="w-5/6 flex-cols mx-20">
+      <div className="w-full sm:w-5/6 mx-auto px-4 sm:px-0">
         <div className="mb-4 text-left px-2 font-semibold">
           <label>{label}</label>
         </div>
-        <div className="flex gap-6">
+        <div className="flex flex-col sm:flex-row gap-6">
           <input
             type="text"
             value={searchTerm}
             onChange={handleInputChange}
             placeholder={placeholder}
-            className="w-full px-4 py-2 border border-gray-300 rounded-3xl shadow-sm sm:text-sm"
+            className="w-full sm:w-2/3 px-4 py-2 border border-gray-300 rounded-3xl shadow-sm sm:text-sm"
           />
           <button
             type="submit"
             className={classNames(
-              "items-center rounded-3xl py-2 px-10",
+              "mt-4 sm:mt-0 items-center rounded-3xl py-2 px-6 sm:px-10",
               btnColor || "bg-black",
               btnTextColor || "text-white"
             )}
