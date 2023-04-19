f89e5b518a80dc6b3d65ad8b30e81a7d14eb391d
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-19 15:40:17 -0700

    Style: add styling for large screen & ternary operator for btn

--- src/components/form/SubscribeForm.jsx
+++ src/components/form/SubscribeForm.jsx
@@ -31,35 +31,42 @@ const SubscribeForm = ({
   return (
     <form
       onSubmit={handleSubmit}
-      className={classNames("w-full px-4 py-8", bgColor || "bg-gray-400")}
+      className={classNames(
+        "w-full px-4 py-8 md:flex md:justify-center md:items-center md:gap-2 md:px-36",
+        bgColor || "bg-gray-400"
+      )}
     >
-      <label className="text-start">
-        <h1 className="text-base font-semibold text-white mb-1">{title}</h1>
-        <p>{description}</p>
-      </label>
-      <div>
-        <input
-          type="text"
-          value={subscribeEmail}
-          onChange={handleInputChange}
-          placeholder={placeholder}
-          className="w-full px-4 py-2 border border-gray-300 rounded-3xl shadow-sm"
-        />
-        {!isValidEmail && (
-          <p className="text-red-500 text-start ml-4">
-            *Please enter a valid email address.
-          </p>
-        )}
-        <button
-          type="submit"
-          className={classNames(
-            "w-full text-base rounded-3xl py-2 mt-2",
-            btnColor || "bg-black",
-            btnTextColor || "text-white"
+      <div className="text-start md:w-1/3">
+        <h1 className="text-lg font-semibold text-white mb-1">{title}</h1>
+        <p className="md:w-3/4">{description}</p>
+      </div>
+      <div className="mt-8 md:mt-4">
+        <div className="md:flex md:gap-2">
+          <input
+            type="text"
+            value={subscribeEmail}
+            onChange={handleInputChange}
+            placeholder={placeholder}
+            className="w-full px-4 py-2 border border-gray-300 rounded-3xl shadow-sm md:w-[484px]"
+          />
+          <button
+            type="submit"
+            className={classNames(
+              "w-full text-base rounded-3xl py-2 mt-2 md:mt-0 md:w-36",
+              btnColor || "bg-black",
+              btnTextColor || "text-white"
+            )}
+          >
+            {isFormSubmitted && isValidEmail ? "Subscribed" : "SIGN UP"}
+          </button>
+        </div>
+        <div className="m-2 w-full h-2">
+          {!isValidEmail && (
+            <p className="text-red-600 text-start ml-4">
+              *Please enter a valid email address.
+            </p>
           )}
-        >
-          {isFormSubmitted ? "Thanks for subscribing!" : "SIGN UP"}
-        </button>
+        </div>
       </div>
     </form>
   );
