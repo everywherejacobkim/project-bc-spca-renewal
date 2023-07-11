7bd93d3a275ce1f9e5401d74729ea7e80e1b4c67
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-07-10 20:35:24 -0700

    Fix: fix importing path typo

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import Image from "next/image";
 import Button from "@/components/common/button";
 import Link from "next/link";
