8c3c2864dcec6dd0a8e6cc1e13afc7cfefd77d97
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-07-20 23:24:08 -0700

    Feat: Add mobile navigation modal and close button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -13,10 +13,19 @@ const Header = () => {
     setIsExpanded(!isExpanded);
   };
 
-  
-  
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsExpanded(false);
+      }
+    };
 
-  
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <header className="bg-white p-4">
@@ -34,37 +43,93 @@ const Header = () => {
             )}
           </button>
         </div>
-        <ul
-          className={`${
-            isExpanded ? "block" : "hidden"
-          } md:flex md:items-center md:justify-end md:flex-1 md:gap-4 font-medium`}
+        <div
+          className={`md:hidden fixed inset-0 ${
+            isExpanded ? "flex" : "hidden"
+          } items-center justify-center bg-white z-10`}
         >
+          <button
+            className="absolute top-12 right-8 md:hidden"
+            onClick={toggleMenu}
+          >
+            <FaTimes className="h-6 w-6" />
+          </button>
+          <ul className="flex flex-col gap-4 font-medium text-center">
+            <li>
+              <Link
+                href="/search-map"
+                className="block text-xl px-2 py-1 text-primary font-bold"
+                onClick={toggleMenu}
+              >
+                Adopt
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/404"
+                className="block text-xl px-2 py-1 hover:text-gray-500"
+                onClick={toggleMenu}
+              >
+                Ways to help
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/404"
+                className="block text-xl px-2 py-1 hover:text-gray-500"
+                onClick={toggleMenu}
+              >
+                Programs & Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/404"
+                className="block text-xl px-2 py-1 hover:text-gray-500"
+                onClick={toggleMenu}
+              >
+                News & Events
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/donation"
+                className="block text-xl px-2 py-1 text-primary font-bold"
+                onClick={toggleMenu}
+              >
+                Donation
+              </Link>
+            </li>
+          </ul>
+          <SearchBar width="w-36" />
+        </div>
+        <ul className="hidden md:flex md:items-center md:justify-end md:flex-1 md:gap-4 font-medium">
           <li>
-            <Link href="/404" className="px-2 py-1 block hover:text-gray-500">
+            <Link href="/404" className="px-2 py-1 hover:text-gray-500">
               Adopt
             </Link>
           </li>
           <li>
-            <Link href="/404" className="px-2 py-1 block hover:text-gray-500">
+            <Link href="/404" className="px-2 py-1 hover:text-gray-500">
               Ways to help
             </Link>
           </li>
           <li>
-            <Link href="/404" className="px-2 py-1 block hover:text-gray-500">
+            <Link href="/404" className="px-2 py-1 hover:text-gray-500">
               Programs & Services
             </Link>
           </li>
           <li>
-            <Link href="/404" className="px-2 py-1 block hover:text-gray-500">
+            <Link href="/404" className="px-2 py-1 hover:text-gray-500">
               News & Events
             </Link>
           </li>
         </ul>
-        <SearchBar width="w-36" />
-       
-           <Link href="/donation">
-          <Button label="Donate" />
+        <div className="hidden md:block">
+          <Link href="/donation">
+            <Button label="Donate" />
           </Link>
+        </div>
       </nav>
     </header>
   );
